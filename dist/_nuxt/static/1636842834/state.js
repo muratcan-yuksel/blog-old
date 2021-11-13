ae4ce1bfe9bf89f80a56786d56ebfcb0555b3a28
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-11-14 02:19:29 +0300

    heroku

--- dist/_nuxt/static/1636842834/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1636842834",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"888db57d"}}}}(null,"\u002F"));
